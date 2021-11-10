8a915318d47a9b8a224ea855e967141ff03408ce
ajogow <ashpujit@gmail.com>
2021-11-09 16:34:04 -0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import random
 
 def my_func1():
     random_n = random.randint(1,100)
-    while random_n != 66:
+    while random_n != 69:
         random_n = random.randint(1,100)
         print(random_n)
 
